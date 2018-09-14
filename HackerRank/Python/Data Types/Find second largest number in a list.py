3b6114477fabe9bb3478eb869304b5ffad6bcd52
Safa Bayar <sbayar2012@outlook.com>
2018-09-14 17:51:34 -0400

    i changed Find second largest number in a list.py

--- HackerRank/Python/Data Types/Find second largest number in a list.py
+++ HackerRank/Python/Data Types/Find second largest number in a list.py
@@ -1,25 +1,13 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-'''
-Problem Statement
+if __name__ == '__main__':
+    n = int(input())
+    arr =list(map(int, input().split(" "))) 
+    k = max(arr)
+    for i in range(0,n):
+        if max(arr) == k:   
+            arr.remove(max(arr))
+    arr.sort(reverse=True)
+    print(arr[0])
 
-Now, lets delve into one of the most basic data types in python, LIST. You are given 'n' numbers. Store them in a list and find the second largest number. Easy enough!
 
-NOTE : Don't use insertion sort
 
-Input Format
 
-First line contains N. Second line contains Array A[] of N integers each separated by a space.
-
-Output Format
-
-Value of the second largest number.
-'''
-
-N=int(raw_input())
-list=list(set(map(int,raw_input().strip().split(" "))))
-list.sort(reverse=True)
-print list[1]
-
-    
-    
